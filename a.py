599fb3d4287f16dacbd54d3ff233e71d99208497
王紫伊 <wangziyi@wangziyideMacBook-Air.local>
2022-12-29 20:42:46 +0800

    add a.py

---
+++ a.py
@@ -0,0 +1,10 @@
+class dog():
+    def __init__(self,weight):
+        self.weight = weight
+    def bark(self):
+        print("wangwang")
+
+if __name__ == '__main__':
+    mydog = dog(5)
+    mydog.bark()
+    print(mydog.weight)
